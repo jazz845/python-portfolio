84348da405823cdba5612eda30079029fd41e8fc
Jaime <contacto.jaimezapata@gmail.com>
2026-06-13 15:23:44 -0500

    Fase 1 - variables y tipos de datos

---
+++ datos_personales.py
@@ -0,0 +1,16 @@
+full_name = "Jaime Zapata"
+id_number = 12345678
+birth_date = "1985-01-30"
+age = 41
+stature = 1.80
+weight = 96
+gender = "Masculino"
+marital_status = "Divorciado"
+nationality = "Colombiana"
+address = "Calle 123 #45-67"
+city = "Medellin"
+phone_number = "3234910156"
+email = "contacto.jaimezapata@gmail.com"
+occupation = "IT engineer"
+company = "Power digital Marketing"
+print(f"Mi nombre completo es {full_name}, mi número de identificación es {id_number}, nací el {birth_date} y tengo {age} años. Mido {stature} metros, peso {weight}kg, soy {gender}, mi estado civil es {marital_status} y mi nacionalidad es {nationality}. Vivo en {address} en la ciudad de {city}, y mi número de teléfono es {phone_number}. Puedes contactarme en {email}. Trabajo como {occupation} en {company}.")
