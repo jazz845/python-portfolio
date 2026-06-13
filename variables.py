d4a9510385e244df6d66b69c8dcbe6b8697e5dbe
Jaime <contacto.jaimezapata@gmail.com>
2026-06-13 15:03:23 -0500

    Fase 1 - variables y tipos de datos

---
+++ variables.py
@@ -0,0 +1,17 @@
+nombre = "Jaime"
+edad = 41
+altura = 1.80
+activo = True
+print(nombre)
+print(edad)
+print(altura)
+print(activo)
+print(type(nombre))
+print(type(edad))
+print(type(altura))
+print(type(activo))
+print("Mi nombre es " + nombre + " y tengo " + str(edad) + " años.")
+print(f"Mi nombre es {nombre} y tengo {edad} años.")
+print(f"Soy {nombre}, tengo {edad} años y mido {altura} metros.")
+edad = edad + 1
+print(f"El año que viene tendré {edad} años.")
